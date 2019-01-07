92cb2a28c7775b72a7b73e86c4df4dab65a3cbf0
anjan111 <socsoftech@gmail.com>
2019-01-07 09:14:16 -0800

    suneel

---
+++ suneel/001_python_basic_syntax/001_basic1.py
@@ -0,0 +1,95 @@
+# this used for to understand basic syntax of the python
+"""
+Python Basic syntax :-
+----------------------
+
+Syntax :-
+-------
+
+----> The rules for writing an instruction in a program 
+
+----> Python Instruction :----
+--------------------------
+
+----> Its a line of code in a program , which is used for to instruct the cpu 
+
+---> there are 2 types 
+
+1. comments :-
+------------- 
+
+----->Whatever the instruction which are not executable by cpu
+
+----> The comments are provided by program
+
+-----> which are used to give the brief explanation for each and every statements , fucntions 
+
+and programs 
+
+
+----> there are two types of comments 
+
+
+1. single line 
+
+--------> at a time we can comment single line 
+
+-------> for that we can use the symbol is   #
+
+-----> A line is bigging with # character it cannot exectable 
+
+
+example :-
+-----------
+
+# single line comment 
+
+
+2. multiple line  :-
+---------------------
+
+---> at a time we can comments multiple lines 
+
+--> we can use '''   '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+2. Keywords:-
+-------------
+
+
+
+
+
+
+
+
+
+
+
+----> Python words :
+
+----> python variable :-
+
+----> python data types
+
+"""
