9875af27111154d97bfece96d4a8190cb0f85359
anjan111 <socsoftech@gmail.com>
2019-01-06 21:53:53 -0800

    Rehaman file

---
+++ Rehaman/001_Basic_understanding/001_basic_syntax.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+# the above line is shabang line ( path of python interpreter)
+
+#single line comment ( by using hash character '#'
+'''
+We can comment multiple lines at a time 
+by using triple quotes 
+
+'''
+
+# the below code understand for statement concept 
+
+'''
+1---> single statemnt 
+
+'''
+# a line which is having single executable instruction
+
+print " this is  single statement example  "
+
+'''
+2----> multiple statement 
+'''
+# a single line which is having more than one executable instruction
+
+print "hello "  ; print "hai" ; print "bye"
+
+
+'''
+3 ---> multiline statement
+'''
+# a single instruction which may contine more than one line 
+
+print "hello \
+	hai \
+	bye "
+
+
+'''
+4---> compound statement / indentation statement 
+
+in othe preogramming language syntax 
+
+block_name
+{
+  st1 
+  ---
+  ---
+}
+
+in python syntax:
+
+
+block_name   :
+    st1
+    st2
+    st3
+    st4
+
+
+'''
+
+if(__name__ == "__main__"):
+   print " i am in if block"
+   print " i am very happy for learning python"
+      print "hai i am error statement"
+
+
