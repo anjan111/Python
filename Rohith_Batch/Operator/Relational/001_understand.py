27657927e1dd4c92d6e532fcc875ea8cba82a4c0
anjan111 <socsoftech@gmail.com>
2019-01-06 19:45:02 -0800

    second commit

---
+++ Rohith_Batch/Operator/Relational/001_understand.py
@@ -0,0 +1,32 @@
+# Relational Opearator
+def main():
+    a  = input (" enter some data ")  # 10
+    b  = input("enter some data ")  # 20
+
+    print a ," lessthan             ", b, " is ",a <  b  # True
+    print a ," greaterthan          ", b, " is ",a >  b  # False 
+    print a ," lessthan or equal    ", b, " is ",a <=  b  # True 
+    print a ," greaterthan or equal ", b, " is ",a >=  b  # True 
+    print a ," eqaul to             ", b, " is ",a ==  b  # True 
+    print a ," not eqaul to         ", b, " is ",a !=  b  # True 
+
+if (__name__ == "__main__"):
+    main()
+'''
+
+examples on relational operators
+'''
+# WAP given number number is even or not
+# WAP given number is divisble by 5 or not
+# wap print true when you eligble for voting 
+
+
+
+
+
+
+
+
+
+
+
---
+++ Rohith_Batch/Operator/Relational/002_even_or_not.py
@@ -0,0 +1,9 @@
+# WAP given number number is even or not
+
+def main():
+    num = input("enter some data ")
+    even = (num % 2 == 0)
+    print num ," iseven ",even
+
+if(__name__ =="__main__"):
+    main()
