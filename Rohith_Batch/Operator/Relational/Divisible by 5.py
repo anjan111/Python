80fe92f3057ac8404054858348cf54654a56fee1
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-08 08:38:36 +0530

    Add files via upload
    Relational Operators examples

---
+++ Rohith_Batch/Operator/Relational/Divisible by 5.py
@@ -0,0 +1,14 @@
+'''
+Program         : Given number is divisible by 5 or not
+
+Programmer      : Rohit
+'''
+
+def main():
+    import math
+    num = input("Please enter the number ")
+    print 'The number is divisible by 5 ', num%5==0
+    
+
+if (__name__ == "__main__"):
+    main()
---
+++ Rohith_Batch/Operator/Relational/Vote Eligibility.py
@@ -0,0 +1,17 @@
+'''
+Program         : Eligible to caste a vote
+
+Programmer      : Rohit
+'''
+
+def main():
+    import math
+    num = input("Please enter your age ")
+    if num<18:
+        print 'You are not eligible to cast a vote'
+    else:
+        print 'You are eligible to cast a vote '
+    
+
+if (__name__ == "__main__"):
+    main()
