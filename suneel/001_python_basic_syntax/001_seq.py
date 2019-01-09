0a48cd1dbff3eb6cfcda060ee0fa2a522e88763e
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-09 11:32:11 +0530

    Add files via upload

---
+++ suneel/001_python_basic_syntax/001_seq.py
@@ -0,0 +1,37 @@
+# data type file
+
+#------------------. Sequential Datatype <------------------------------
+#------------------------------------------------------------------------
+
+str1 = 'soc softech'
+str2 = "soc softech"
+str3 = '''soc softech'''
+str4 = """soc softech"""
+print "str1 ",str1
+print "type of str1",type(str1)
+print "id of str2",id(str1)
+
+list1 =[1,2,3,4,5]
+print list1
+print "list1 ",type(list1)
+print "id ",id(list1)
+
+t =(1,2,3,4,5)
+print t
+print type(t)
+print "id ",id(t)
+
+s1 ={1,2,3,4,5}
+print s1
+print type(s1)
+print "id ",id(s1)
+
+d1 ={45 :"anjan" ,67:"rehaman", 89:"suneel"}
+print d1
+print type(d1)
+print "id ",id(d1)
+
+
+
+
+
