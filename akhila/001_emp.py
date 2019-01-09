85232fc627ea7af1cc9bdb9016dd7d5f0da0934f
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-09 17:41:12 +0530

    Add files via upload

---
+++ akhila/001_emp.py
@@ -0,0 +1,22 @@
+class Emp_det():
+    mail ="anjan@gmail.com"   #   class variable
+    def role(self):           # method
+        print "self ",self
+        print "Software Engineer"
+
+
+em_ob  = Emp_det()
+print em_ob
+em_ob.role()
+em_ob1 = Emp_det()
+print em_ob1
+em_ob1.role()
+
+
+
+
+
+        
+
+    
+
