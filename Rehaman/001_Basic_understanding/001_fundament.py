701f7eeb8b3331370edd9d369121ca9044810886
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-09 11:32:47 +0530

    Add files via upload

---
+++ Rehaman/001_Basic_understanding/001_fundament.py
@@ -0,0 +1,37 @@
+# Data types file
+
+# ---------------->  fundamental datatype <-------------------------
+#-------------------------------------------------------------------
+from time  import sleep
+a = 123
+print "a = ", a
+print " type of a ", type(a)
+print " id of a", id(a)
+
+sleep(3)
+a = 123.789
+print "a = ", a
+print " type of a ", type(a)
+print " id of a", id(a)
+
+
+sleep(3)
+a = True 
+print "a = ", a
+print " type of a ", type(a)
+print " id of a", id(a)
+sleep(3)
+a = False
+print "a = ", a
+print " type of a ", type(a)
+print " id of a", id(a)
+sleep(3)
+a = 123 + 78j
+print "a = ", a
+print " type of a ", type(a)
+print " id of a", id(a)
+sleep(3)_
+a = None
+print "a = ", a
+print " type of a ", type(a)
+print " id of a", id(a)
