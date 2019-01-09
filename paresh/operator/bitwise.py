cf741c903bc793d3267104a9a3b3f45f15ca94bb
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-09 18:53:07 +0530

    Add files via upload

---
+++ paresh/operator/bitwise.py
@@ -0,0 +1,38 @@
+>>> 5 & 6   # bitwise and between 5 , 6
+4
+>>> 5 | 6   # bit wise or 
+7
+>>> 5 ^ 6    # bitwise xor
+3
+>>> 125 << 4  # left shift
+2000
+>>> bin(125)  # bin function on 125
+'0b1111101'
+>>> bin(2000)  # bin function on 2000
+'0b11111010000'
+>>> 125 <<2    # 2 bits shifting to left side 
+500
+>>> 2000>> 2   # 2 bits shifting to right side 
+500
+>>> ~125     # bit wise not on 125 
+-126
+>>> ~4
+-5
+>>> 
+
+# check 5 bit position  of 159 is 0 or 1
+# toggle the 5 bit position  of 159   1<---->0
+# clear the 5 bit position  of 159    1, 0 ----> 0
+# set the 5 bit position  of 159       1,0   ----> 1
+#given number iseven or not using bit wise operator
+
+# find given number is power of 2 or not --->
+
+1 ,2 ,4,8,16,32,64,128,256
+
+
+
+
+
+
+
