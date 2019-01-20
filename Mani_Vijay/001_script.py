11de7836c14d071c4c268fc826e844db2aed7362
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-19 19:59:58 -0800

    Add files via upload

---
+++ Mani_Vijay/001_script.py
@@ -0,0 +1,31 @@
+# script mode  //  batch mode 
+'''
+----> Python Statements we are write  into one file
+save as .py extension
+----> then we have to run the file
+
+----> then we can get output on file shell
+'''
+# reusability 
+print "hello"
+print "hai"
+print " this is script mode"
+
+
+'''
+
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # interactive mode
+>>> # we are directly executing python statements on python shell
+>>> print "hello"
+hello
+>>> print "hai"
+hai
+>>> print " interactive mode"
+ interactive mode
+>>> # if we close this shell we can lost previous execution status
+>>> # one time execution
+>>> # shell mode
+
+'''
