f46824768156e0a149e4815db930fd6decb1070d
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-20 19:43:11 -0800

    Add files via upload

---
+++ Rohith_Batch/loops/002_for_seq.py
@@ -0,0 +1,14 @@
+# wap print elements in a list
+def main():
+    num = input("enter any decimal num")
+    for i in range(1,11):
+        print num , " * " ,i ," = ",num*i
+    else :
+        print "whenever no more updatation"
+if(__name__ == "__main__"):
+    main()
+
+# wap to find no of elements in any sequence
+# wap to find sum of elements in a seq
+# wap to find multiplication table in for loop
+
