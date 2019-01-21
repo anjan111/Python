3dbccda15bfdf307fd29af7ccf285674724861b4
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-20 18:27:36 -0800

    Add files via upload

---
+++ Mani_Vijay/002_basic_syntax.py
@@ -0,0 +1,17 @@
+# this is single line comment
+'''
+if any thing which is enclosed
+with in the triple quotes
+that is multiple line comment 
+'''
+if( __name__ == "__main__"):
+    
+    print "this is single statement"  # this is single statement
+
+    print "this is inst1 " ; print "this is ist2" ; print "this is inst3" # multple
+
+    print "this is \
+        instruction \
+        but multiple lines "
+    #multiline statement
+
