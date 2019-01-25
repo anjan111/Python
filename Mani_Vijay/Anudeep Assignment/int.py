dc5251cff124c1558b6a2bbe552c83b8b3457f6b
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-25 13:48:04 +0530

    Add files via upload

---
+++ Mani_Vijay/Anudeep Assignment/int.py
@@ -0,0 +1,10 @@
+
+def main():
+    num =input("enter some data ")
+    num =int(num)
+    print " num  = ",num
+    print "type is ", type(num)
+    print "id   is ",id(num)
+
+if(__name__ == "__main__"):
+    main()
