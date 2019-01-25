44682c4c522b34b298245bfdead4acf1eb2ad701
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-24 17:28:41 -0800

    Add files via upload

---
+++ Mani_Vijay/python buit-in functions/003_input.py
@@ -0,0 +1,10 @@
+# wap to understand the input function
+
+def main():
+    num =input("enter some data ")
+    print " num  = ",num
+    print "type is ", type(num)
+    print "id   is ",id(num)
+
+if(__name__ == "__main__"):
+    main()
---
+++ Mani_Vijay/python buit-in functions/004_in_int.py
@@ -0,0 +1,13 @@
+# wap read  any data from input and convert int only
+
+def main():
+    num =input("enter some data ")
+    num =int(num)
+    print " num  = ",num
+    print "type is ", type(num)
+    print "id   is ",id(num)
+
+if(__name__ == "__main__"):
+    main()
+
+# above code  run 9 time with 9 different datatypes 
