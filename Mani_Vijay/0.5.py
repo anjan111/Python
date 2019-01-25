ecd15917135d5837044834395cd54b2c572bc12c
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-24 19:58:53 -0600

    Delete 0.5.py

--- Mani_Vijay/0.5.py
+++
@@ -1,9 +0,0 @@
-def main():
-    num =input("enter some data ")
-    num =float(num)
-    print " num  = ",num
-    print "type is ", type(num)
-    print "id   is ",id(num)
-
-if(__name__ == "__main__"):
-    main()
