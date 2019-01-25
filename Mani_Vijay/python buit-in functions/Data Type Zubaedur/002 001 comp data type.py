a53d53ac8dafef5c8e354d4790488cffcaf1f252
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-26 04:05:43 +0530

    Add files via upload

---
+++ Mani_Vijay/python buit-in functions/Data Type Zubaedur/002 001 comp data type.py
@@ -0,0 +1,49 @@
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = str(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = list(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = tuple(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = set(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
---
+++ Mani_Vijay/python buit-in functions/Data Type Zubaedur/002 comp data type.py
@@ -0,0 +1,61 @@
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = int(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = float(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = complex(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = bool(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+# wap addition of two numbers using raw_input functions 
+
+def main( ):
+    var = raw_input("enter some data")
+    var = NoneType(var)
+    print "var = ",var
+    print " type of var ",type(var)
+    print " id of var ",id(var)
+
+if(__name__ == "__main__"):
+        main()
+
+
