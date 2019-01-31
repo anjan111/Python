54ebc80ae8b7ee388f68715a64000f2eb66404d5
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-01-31 08:25:24 +0530

    Add files via upload

---
+++ sunny_venkat_aswini/python_introduction/001_script_mode.py
@@ -0,0 +1,11 @@
+# script mode / batch mode
+'''
+---> create a  new file / notepad /python ide
+---> write the python statements
+---> save as .py extension
+---> run the program
+
+---> reusability 
+'''
+print "hello"
+print "hai"
---
+++ sunny_venkat_aswini/python_introduction/002_interactive.py
@@ -0,0 +1,26 @@
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # we have to execute python statemnt on python shell directly
+>>> # interavtive mode
+>>> # we can get the output on shell
+>>> a =10
+>>> print a
+10
+>>> a
+10
+>>> b=10
+>>> a+b
+20
+>>> a-b
+0
+>>> # bedugging / testing
+>>> # debugging / testing
+>>> 
+>>> 
+>>> type(a)
+<type 'int'>
+>>> a =46245768786768768767
+>>> type(a)
+<type 'long'>
+>>> id(a)
+45932752L
