74de676770ae09ef4bdf07a5bcd3cec50d4e5904
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-02-12 20:26:02 +0530

    Add files via upload

---
+++ Sai_Krishna/002_python Basic Function/007_input_float.py
@@ -0,0 +1,14 @@
+from time import sleep 
+var = input("Enter  some  data ")
+
+print "data in var ",var  # print statement
+print "type of var ",type(var)
+print("id of var ",id(var)) #print function
+
+var =float(var)
+sleep(5)
+print "data in var ",var  # print statement
+print "type of var ",type(var)
+print("id of var ",id(var)) #print function
+
+# how many data type elements can convert into float
---
+++ Sai_Krishna/002_python Basic Function/assignment.py
@@ -0,0 +1,23 @@
+# raw_input ---> display --->type ---> id
+'''
+the above logic run 10 times  each time new data element
+'''
+#datatype --->  raw_input ---> display data  --->type ---> id ---> particular datatype 
+'''
+the above logic write  10 times  each time new datatype function
+'''
+# input ---> display data  --->type ---> id
+'''
+the above logic run 10 times  each time new data element
+'''
+#any data type --->  input ---> display data  --->type ---> id ---> particular datatype 
+'''
+the above logic write 9 times  and 9 run  each time new datatype function
+'''
+# convert any number to binary
+# convert any number to octal
+# convert any number to decimal
+# convert any number to hexa decimal
+
+
+
