39b7a0cc7a10e053c8ce6e73a316fda3c05f9afc
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-02-16 18:29:04 +0530

    Add files via upload

---
+++ Deepa_Ramu/001_python syntax/001_comment.py
@@ -0,0 +1,14 @@
+# this is single line comment
+'''
+this is
+multiple
+line
+comment
+
+'''
+# what is the use of the comments ?
+'''
+The main use of the code is
+to give the brief explanation for
+each and every statement in a program
+'''
---
+++ Deepa_Ramu/001_python syntax/002_statement.py
@@ -0,0 +1,70 @@
+# single statement
+'''
+A single  line it may have single executable instruction
+'''
+#print "hello"
+
+
+# multiple statement
+'''
+A single  line it may have more than one  executable instructions
+'''
+#print "hello" ; print "hai" ; print "bye"
+
+# multiline statement
+'''
+A single executable instruction it may have continue more than one line 
+'''
+'''
+print 'hello\n\
+hai\n\
+bye'
+'''
+# compuund statement
+'''
+The N no of executable instruction
+we have to write with in a  block
+
+The group of statements as a block
+
+other pl:
+--------
+block_name
+{
+ ----
+ ----
+}
+
+In python ,
+
+block_name :
+    statement
+    ---------
+    --------
+    ---------
+
+if(12 > 34):
+    print"hello"
+    print"hai"
+    print"bye"
+
+'''
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
