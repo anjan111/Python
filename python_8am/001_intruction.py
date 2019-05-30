35f96af453d8e50f32428c206bb0f1a6d9e9ece4
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-05-30 08:56:07 +0530

    Add files via upload

---
+++ python_8am/001_intruction.py
@@ -0,0 +1,51 @@
+# single line comment
+
+'''
+At time we hide only
+single line   ( # )
+
+'''
+# multiline comments
+'''
+The multiple n no of
+
+line it write with in triple quote
+'''
+# to give the brief explanation for each and every statement / function
+
+print("Hello !") # single statement
+print("How are you")  # single statement
+
+# single statement 
+'''
+A single line which having single executable instruction
+'''
+# multiple statements
+'''
+A single line which may have one than one executable instrucntion
+'''
+print("I am Anjan Kumar") ; print("I am AI Engineer") ;print("I am Developing Opython IDE")
+
+# multiline statement
+'''
+A single executable instruction can continue more than one line 
+
+'''
+print("Hello All of you \
+I am Anjan Kumar \
+I am trying share knowledge of Python to Guys ")
+
+# compound Statement
+'''
+We can create a group of executable instruction as a one block
+
+'''
+# syntax:
+'''
+Block_name  :
+    statement
+    ---------
+    ------
+    ---------
+    --------
+'''
---
+++ python_8am/002_python_keywords.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import keyword
+>>> keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+>>> len(keyword.kwlist)
+35
+>>> # based topic we have to discuss the above all keywords 
