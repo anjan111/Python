b0caa43bf042d3a338e2368a5c30305146d114c8
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-05-31 22:19:06 -0500

    Add files via upload

---
+++ python_8am/002_python_built function/001_raw_input.py
@@ -0,0 +1,20 @@
+# raw_input function it can work in python
+'''
+raw_input is used for to read any data from keyboard
+
+but it can return str datatype
+'''
+
+var = raw_input("enter some data : ")
+
+print "var ",var   # print statement
+
+print(type(var))  # print function
+
+print "id  :",id(var)
+
+
+'''
+execute above logic 10 time per each time enter new data
+
+'''
---
+++ python_8am/002_python_built function/002_input.py
@@ -0,0 +1,24 @@
+# input function
+
+'''
+input function we can enter data it can return
+
+corresponding sped=cific datatype
+'''
+
+var = input("enter some data : ")
+print "var : ",var
+print type(var)
+print "id  : ",id(var)
+
+
+'''
+execute 10 times per each time enter new daata type element
+'''
+
+# data type conversion function
+
+# number system conversion
+
+# other built functions
+
