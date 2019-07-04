cafdc9a99b19bad1b423bb7734b580db1247225e
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-07-04 07:21:12 +0530

    Add files via upload

---
+++ Palani_Bishwo_apoorva/001_Python_Basic_syntax/001_instructions.py
@@ -0,0 +1,50 @@
+# single line comment
+'''
+This
+is
+multiple line comment
+'''
+
+# statements
+'''
+----> an instructiosn which can executable
+
+1. single statement
+
+2. Multiple statement
+
+3. Multiline statement
+
+4. Compound statement
+'''
+
+# single statement
+
+print "hello"  # printing hello on monitor
+
+# multiple stateements
+
+print "hai" ; print "i am anjan kumar" ; print "bye"
+
+# Multiline statement
+
+print "hello  \
+hai  \
+bye"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Palani_Bishwo_apoorva/001_sample.py
@@ -0,0 +1,4 @@
+a =10
+b =20
+print a+b
+print a-b
