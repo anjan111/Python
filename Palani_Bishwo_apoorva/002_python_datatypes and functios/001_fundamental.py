1b762de016807ca9a0ed6472cf36b51da22d20e9
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-07-05 07:10:38 +0530

    Add files via upload

---
+++ Palani_Bishwo_apoorva/002_python_datatypes and functios/001_fundamental.py
@@ -0,0 +1,6 @@
+# to understand the fundamental datatype and basic built in function
+
+var = input("enter some fundamental data  :")
+print "data with in var : ",var
+print (type(var))
+print "memory location :",id(var)
---
+++ Palani_Bishwo_apoorva/002_python_datatypes and functios/002_sequential.py
@@ -0,0 +1,10 @@
+# to understand the sequential datatype and basic built in function
+
+var = input("enter some sequential data  :")
+print "data with in var : ",var
+print (type(var))
+print "memory location :",id(var)
+print "no of elements  ",len(var)
+print "smallest element: ",min(var)
+print "biggest element : ",max(var)
+print "sum of elements : ",sum(var)# sum fun it won't for string datatype
